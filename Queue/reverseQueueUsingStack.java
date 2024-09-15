608f0a16504a53b36a1f2a611716e42466fabdbb
Parth Srivastava <134224360+luxorishere@users.noreply.github.com>
2024-09-15 18:02:56 +0530

    Update reverseQueueUsingStack.java

--- Queue/reverseQueueUsingStack.java
+++ Queue/reverseQueueUsingStack.java
@@ -1,4 +1,12 @@
- public static void reverseQueue(Queue<Integer> queue){
+// 1. **Use a Stack:**
+//    - Push each element from the queue onto a stack to reverse their order
+
+// 2. **Repopulate the Queue:**
+//    - Pop elements from the stack back into the queue
+
+// this leverages the LIFO property of stacks to reverse the queue efficiently
+
+public static void reverseQueue(Queue<Integer> queue){
         Stack<Integer> stack = new Stack<>();
         System.out.println(queue);
         while (!queue.isEmpty()) {
