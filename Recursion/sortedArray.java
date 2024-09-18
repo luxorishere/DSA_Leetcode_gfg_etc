7b1de49593055ad165e6cebcbef1d5dd749e1736
Parth Srivastava <134224360+luxorishere@users.noreply.github.com>
2024-09-18 20:50:52 +0530

    Create sortedArray.java

---
+++ Recursion/sortedArray.java
@@ -0,0 +1,10 @@
+    public static boolean sortedArray(int[] arr, int index){
+        if (index == arr.length - 1){
+            return arr[index] >= arr[index - 1];
+
+        }
+        else if (arr[index] > arr[index + 1]){
+            return false;
+        }
+        return sortedArray(arr, index + 1);
+    }
