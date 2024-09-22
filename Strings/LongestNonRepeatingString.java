184c4e3c6b0661545fe17c0579bc594b9b1b9ff2
Parth Srivastava <134224360+luxorishere@users.noreply.github.com>
2024-09-22 17:27:57 +0530

    Update LongestNonRepeatingString.java

--- Strings/LongestNonRepeatingString.java
+++ Strings/LongestNonRepeatingString.java
@@ -33,4 +33,40 @@ public static String longestNonRepeatingString(String string){
             }
             return maxString;
         }
+ public static String longestNonRepeating(String string) {
+            if (string.length() == 1){
+                return string;
+            }
+            HashMap<Character, Boolean> hashMap = new HashMap<>();
 
+            StringBuilder stringBuilder = new StringBuilder();
+            String maxString = "";
+
+            for (int i = 0; i < string.length() - 1; i++) {
+                if (hashMap.containsKey(string.charAt(i + 1))) {
+                    hashMap.put(string.charAt(i), true);
+                    stringBuilder.append(string.charAt(i));
+                    if (stringBuilder.length() > maxString.length()) {
+                        maxString = stringBuilder.toString();
+
+
+                    }
+                    hashMap.clear();
+                    stringBuilder = new StringBuilder();
+
+                } else {
+
+
+
+                    hashMap.put(string.charAt(i), true);
+                    stringBuilder.append(string.charAt(i));
+                }
+            }
+            if (!hashMap.containsKey(string.charAt(string.length() - 1))){
+                stringBuilder.append(string.charAt(string.length() - 1));
+            }
+            if (stringBuilder.length() > maxString.length()){
+                maxString = stringBuilder.toString();
+            }
+            return maxString;
+        }
